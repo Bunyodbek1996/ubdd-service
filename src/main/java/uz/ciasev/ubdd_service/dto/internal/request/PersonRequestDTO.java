ceb6011f5fee00f57df715970270b456ba9a55af
Bunyod Pro <bunyodguru@gmail.com>
2024-08-29 11:09:02 +0500

    some feature added

--- src/main/java/uz/ciasev/ubdd_service/dto/internal/request/PersonRequestDTO.java
+++ src/main/java/uz/ciasev/ubdd_service/dto/internal/request/PersonRequestDTO.java
@@ -10,7 +10,6 @@ import uz.ciasev.ubdd_service.entity.dict.person.CitizenshipType;
 import uz.ciasev.ubdd_service.entity.dict.person.Gender;
 import uz.ciasev.ubdd_service.entity.dict.person.Nationality;
 import uz.ciasev.ubdd_service.exception.ErrorCode;
-import uz.ciasev.ubdd_service.utils.validator.ActiveOnly;
 import uz.ciasev.ubdd_service.utils.validator.ValidAddress;
 
 import javax.validation.Valid;
@@ -25,25 +24,25 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class PersonRequestDTO {
 
-    @Size(max = 50, message = ErrorCode.MAX_FIRST_NAME_KIR_LENGTH)
+    @Size(max = 100, message = ErrorCode.MAX_FIRST_NAME_KIR_LENGTH)
     @NotBlank(message = ErrorCode.FIRST_NAME_KIR_REQUIRED)
     private String firstNameKir;
 
-    @Size(max = 50, message = ErrorCode.MAX_SECOND_NAME_KIR_LENGTH)
+    @Size(max = 100, message = ErrorCode.MAX_SECOND_NAME_KIR_LENGTH)
     private String secondNameKir;
 
-    @Size(max = 50, message = ErrorCode.MAX_LAST_NAME_KIR_LENGTH)
+    @Size(max = 100, message = ErrorCode.MAX_LAST_NAME_KIR_LENGTH)
     @NotBlank(message = ErrorCode.LAST_NAME_KIR_REQUIRED)
     private String lastNameKir;
 
-    @Size(max = 50, message = ErrorCode.MAX_FIRST_NAME_LAT_LENGTH)
+    @Size(max = 100, message = ErrorCode.MAX_FIRST_NAME_LAT_LENGTH)
     @NotBlank(message = ErrorCode.FIRST_NAME_LAT_REQUIRED)
     private String firstNameLat;
 
-    @Size(max = 50, message = ErrorCode.MAX_SECOND_NAME_LAT_LENGTH)
+    @Size(max = 100, message = ErrorCode.MAX_SECOND_NAME_LAT_LENGTH)
     private String secondNameLat;
 
-    @Size(max = 50, message = ErrorCode.MAX_LAST_NAME_LAT_LENGTH)
+    @Size(max = 100, message = ErrorCode.MAX_LAST_NAME_LAT_LENGTH)
     @NotBlank(message = ErrorCode.LAST_NAME_LAT_REQUIRED)
     private String lastNameLat;
 
@@ -56,17 +55,14 @@ public class PersonRequestDTO {
     private AddressRequestDTO birthAddress;
 
     @NotNull(message = ErrorCode.CITIZENSHIP_TYPE_REQUIRED)
-    @ActiveOnly(message = ErrorCode.CITIZENSHIP_TYPE_DEACTIVATED)
     @JsonProperty(value = "citizenshipTypeId")
     private CitizenshipType citizenshipType;
 
     @NotNull(message = ErrorCode.GENDER_REQUIRED)
-    @ActiveOnly(message = ErrorCode.GENDER_DEACTIVATED)
     @JsonProperty(value = "genderId")
     private Gender gender;
 
     @NotNull(message = ErrorCode.NATIONALITY_REQUIRED)
-    @ActiveOnly(message = ErrorCode.NATIONALITY_DEACTIVATED)
     @JsonProperty(value = "nationalityId")
     private Nationality nationality;
 
